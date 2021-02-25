65508e98bf6dc315af0ff362640813874dc6a9ad
Kevin Lynch <kevin.e.lynch89@gmail.com>
2021-02-25 23:53:42 +0000

    python script writes prism file

--- code/prism/possession.pm
+++ code/prism/possession.pm
@@ -1,28 +1,77 @@
-dtmc
-
-module possession
-
-    // state: i.e. area of the pitch where ball is in possession
-    s : [0..8] init {possession begins}
-        // 0 - 6: areas of the pitch
-        // 7    : posession lost
-        // 8    : goal scored
-
-    [] s=0 -> {zero_to_zero}  : (s'=0) + {zero_to_one}  : (s'=1) + {zero_to_two}  : (s'=2) + {zero_to_three}  : (s'=3) + {zero_to_four}  : (s'=4) + {zero_to_five}  : (s'=5) + {zero_to_six}  : (s'=6) + {zero_to_PL}  : (s'=7) + {zero_to_G}  : (s'=8);
-    [] s=1 -> {one_to_zero}   : (s'=0) + {one_to_one}   : (s'=1) + {one_to_two}   : (s'=2) + {one_to_three}   : (s'=3) + {one_to_four}   : (s'=4) + {one_to_five}   : (s'=5) + {one_to_six}   : (s'=6) + {one_to_PL}   : (s'=7) + {one_to_G}   : (s'=8);
-    [] s=2 -> {two_to_zero}   : (s'=0) + {two_to_one}   : (s'=1) + {two_to_two}   : (s'=2) + {two_to_three}   : (s'=3) + {two_to_four}   : (s'=4) + {two_to_five}   : (s'=5) + {two_to_six}   : (s'=6) + {two_to_PL}   : (s'=7) + {two_to_G}   : (s'=8);
-    [] s=3 -> {three_to_zero} : (s'=0) + {three_to_one} : (s'=1) + {three_to_two} : (s'=2) + {three_to_three} : (s'=3) + {three_to_four} : (s'=4) + {three_to_five} : (s'=5) + {three_to_six} : (s'=6) + {three_to_PL} : (s'=7) + {three_to_G} : (s'=8);
-    [] s=4 -> {four_to_zero}  : (s'=0) + {four_to_one}  : (s'=1) + {four_to_two}  : (s'=2) + {four_to_three}  : (s'=3) + {four_to_four}  : (s'=4) + {four_to_five}  : (s'=5) + {four_to_six}  : (s'=6) + {four_to_PL}  : (s'=7) + {four_to_G}  : (s'=8);
-    [] s=5 -> {five_to_zero}  : (s'=0) + {five_to_one}  : (s'=1) + {five_to_two}  : (s'=2) + {five_to_three}  : (s'=3) + {five_to_four}  : (s'=4) + {five_to_five}  : (s'=5) + {five_to_six}  : (s'=6) + {five_to_PL}  : (s'=7) + {five_to_G}  : (s'=8);
-    [] s=6 -> {six_to_zero}   : (s'=0) + {six_to_one}   : (s'=1) + {six_to_two}   : (s'=2) + {six_to_three}   : (s'=3) + {six_to_four}   : (s'=4) + {six_to_five}   : (s'=5) + {six_to_six}   : (s'=6) + {six_to_PL}   : (s'=7) + {six_to_G}   : (s'=8);
-    
-    // absorbing states
-    [] s=7 -> (s'=7);
-    [] s=8 -> (s'=8);
-
-endmodule
-
-rewards 
-    s = 7 : 1;
-endrewards
+"dtmc\n\n
+module possession\n\n
+\t// state: i.e. area of the pitch where ball is in possession\n
+\ts : [0..8] init {possession begins}\n
+\t\t// 0 - 6: areas of the pitch\n
+\t\t// 7    : posession lost\n
+\t\t// 8    : goal scored\n\n
+\t[] s=0 -> {" + str(trans_mat[0][0]) + "} : (s'=0) + {" + 
+    str(trans_mat[0][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[0][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[0][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[0][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[0][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[0][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[0][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[0][8]) + "}  : (s'=8);\n\t
+    [] s=1 -> {" + str(trans_mat[1][0]) + "} : (s'=0) + {" + 
+    str(trans_mat[1][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[1][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[1][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[1][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[1][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[1][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[1][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[1][8]) + "}  : (s'=8);\n\t
+    [] s=2 -> {" + str(trans_mat[2][0]) + "} : (s'=0) + {" + 
+    str(trans_mat[2][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[2][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[2][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[2][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[2][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[2][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[2][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[2][8]) + "}  : (s'=8);\n\t
+    [] s=3 -> {" + str(trans_mat[3][0]) + "} : (s'=0) + {" + 
+    str(trans_mat[3][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[3][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[3][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[3][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[3][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[3][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[3][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[3][8]) + "}  : (s'=8);\n\t
+    [] s=4 -> {" + str(trans_mat[4][0]) + "} : (s'=0) + {" + 
+    str(trans_mat[4][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[4][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[4][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[4][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[4][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[4][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[4][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[4][8]) + "}  : (s'=8);\n\t
+    [] s=5 -> {" + str(trans_mat[5][0]) + "}  : (s'=0) + {" + 
+    str(trans_mat[5][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[5][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[5][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[5][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[5][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[5][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[5][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[5][8]) + "}  : (s'=8);\n\t
+    [] s=6 -> {" + str(trans_mat[6][0]) + "} : (s'=0) + {" + 
+    str(trans_mat[6][1]) + "}  : (s'=1) + {" + 
+    str(trans_mat[6][2]) + "}  : (s'=2) + {" + 
+    str(trans_mat[6][3]) + "}  : (s'=3) + {" + 
+    str(trans_mat[6][4]) + "}  : (s'=4) + {" + 
+    str(trans_mat[6][5]) + "}  : (s'=5) + {" + 
+    str(trans_mat[6][6]) + "}  : (s'=6) + {" + 
+    str(trans_mat[6][7]) + "}  : (s'=7) + {" + 
+    str(trans_mat[6][8]) + "}  : (s'=8);\n
+\n
+\t// absorbing states\n
+\t[] s=7 -> (s'=7);\n
+\t[] s=8 -> (s'=8);\n
+\n
+endmodule"
 
